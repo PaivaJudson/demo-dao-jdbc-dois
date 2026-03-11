1c27b91e9e806dc8390a98647c10a5155d4461a8
Judson Paiva <judson.paiva@hotmail.com>
2026-03-11 23:55:52 +0100

    project configuration

---
+++ src/main/java/org/example/db/DB.java
@@ -0,0 +1,78 @@
+package org.example.db;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class DB {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Properties properties = loadProperties();
+                String url = properties.getProperty("db.url");
+                String username = properties.getProperty("db.username");
+                String password = properties.getProperty("db.password");
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    private static Properties loadProperties(){
+
+        try {
+            Properties properties = new Properties();
+            String fileName = "application.properties";
+            var stream = DB.class.getClassLoader().getResourceAsStream(fileName);
+
+            if(stream == null){
+                throw new DbException("Ficheiro " + fileName + " nao encontrado ");
+            }
+
+            properties.load(stream);
+            return properties;
+
+        }catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement statement){
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet resultSet){
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+
+
+
+}

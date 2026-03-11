7954829265a57bb2bae54088253f512890bbf22b
Judson Paiva <judson.paiva@hotmail.com>
2026-03-12 00:17:55 +0100

    implementation seller

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,18 @@
 package org.example;
 
 import org.example.model.entities.Department;
+import org.example.model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     static void main() {
 
-        Department department = new Department();
+        Department department = new Department(1, "Books");
         IO.println(department);
+
+        Seller seller = new Seller(21, "Hudson", "hudson@gmail.com", new Date(), 3000000.0, department);
+        IO.println(seller);
+
     }
 }
